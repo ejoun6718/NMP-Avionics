664cd89043fa663bd9f9db18382c224a097dff03
ejoun6718 <hjoun@ucsd.edu>
2017-12-02 12:45:35 -0800

    Update graphdata.py

--- graphdata.py
+++ graphdata.py
@@ -2,22 +2,26 @@ import matplotlib.pyplot as plt
 
 altitude = []
 speed = []
+intervals = []
+i = 0
 with open('test.txt') as file:
         for line in file:
                 newAltitude, newSpeed = line.split()
                 altitude.append(newAltitude)
                 speed.append(newSpeed)
+                intervals.append(i)
+                i = i + 0.5
 
                 print('Altitude: ' + newAltitude)
                 print('Speed: ' + newSpeed + '\n')
 
 plt.figure(1)
-plt.plot(altitude)
+plt.plot(intervals, altitude)
 plt.ylabel('Altitude')
 plt.xlabel('Time')
 
 plt.figure(2)
-plt.plot(speed)
+plt.plot(intervals, speed)
 plt.ylabel('Speed')
 plt.xlabel('Time')
 plt.show()
